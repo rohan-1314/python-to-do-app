3338d9b887580dc6cd92c60633f1bae6f50289fc
rohan <rohan.vagadiya14@gmail.com>
2022-12-19 21:51:15 +0530

    implement the edit ,complete,exit feature and listbox

--- gui.py
+++ gui.py
@@ -5,9 +5,18 @@ label = sg.Text("Enter a TODO")
 input_text = sg.InputText(tooltip="Enter todo",
                           key='todo')
 add_butt = sg.Button('add')
+
+list_box = sg.Listbox(values=fn.get_todo()
+                      , enable_events=True,
+                      key='todos', size=[45, 10])
+edit_but = sg.Button('edit')
+complete_but = sg.Button('complete')
+exit_but = sg.Button('exit')
 window = sg.Window("my todo-list",
                    layout=[[label],
-                           [input_text, add_butt]],
+                           [input_text, add_butt],
+                           [list_box, edit_but,complete_but],
+                           [exit_but]],
                    font=('Helvetica', 12))
 while True:
     event, values = window.read()
@@ -19,6 +28,25 @@ while True:
             new_todo = values['todo'] + '\n'
             todos.append(new_todo)
             fn.write_todo(todos)
+            window['todos'].update(values=todos)
+        case 'edit':
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+            todos = fn.get_todo()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            fn.write_todo(todos)
+            window['todos'].update(values=todos)
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
+        case 'complete':
+            todos = fn.get_todo()
+            index = todos.index(values['todos'][0])
+            todos.pop(index)
+            fn.write_todo(todos)
+            window['todos'].update(values=todos)
+        case 'exit':
+            break
         case sg.WIN_CLOSED:
             break
 
